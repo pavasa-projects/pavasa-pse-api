48a43f205a094e0e15557208146fe73fc2be172b
Rakesh Kavitake <rakesh.kavitake@gmail.com>
2021-06-13 00:12:54 +0530

    first

---
+++ src/main/java/com/pavasa/pse/api/PavasaPseApiApplication.java
@@ -0,0 +1,13 @@
+package com.pavasa.pse.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavasaPseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PavasaPseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pavasa/pse/api/PavasaPseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavasa.pse.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PavasaPseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
